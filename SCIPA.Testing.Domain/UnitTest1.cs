051d7ac6fc46522c50caaf8603e74c6610ff11ea
Will Whitehead <whitehead.williamthomas@gmail.com>
2016-02-26 01:00:13 +0000

    Addition of Business Intelligence and Testing projects.
    Three BI projects (Reporting, Integration and Mining) and three test
    projects (Data, Domain and Interfaces).

---
+++ SCIPA.Testing.Domain/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SCIPA.Testing.Domain
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
