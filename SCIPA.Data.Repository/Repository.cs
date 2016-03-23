e9b59ce4fd26a12fc4dcb5c4b2ad9456a35a98e4
Will Whitehead <whitehead.williamthomas@gmail.com>
2016-03-23 09:36:04 +0000

    Updated the data repository methods.

--- SCIPA.Data.Repository/Repository.cs
+++ SCIPA.Data.Repository/Repository.cs
@@ -170,45 +170,182 @@ namespace SCIPA.Data.Repository
         public void UpdateDatabaseCommunicator(DOM.DatabaseCommunicator databaseCommunicator)
         {
             var dbValue = _db.Communicators.FirstOrDefault(comm => comm.Id == databaseCommunicator.Id);
-
+            if (dbValue == null) return;
+            dbValue = _mapper.Map(databaseCommunicator, new DAL.DatabaseCommunicator());
+            _db.SaveChanges();
         }
 
         public void DeleteDatabaseCommunicator(DOM.DatabaseCommunicator databaseCommunicator)
         {
-            throw new System.NotImplementedException();
+            var dbValue = RetrieveCommunicator(databaseCommunicator.Id);
+            if (dbValue == null) return;
+            _db.Communicators.Remove(_mapper.Map(databaseCommunicator, new DAL.DatabaseCommunicator()));
+            _db.SaveChanges();
         }
 
         public void CreateFileCommunicator(DOM.FileCommunicator fileCommunicator)
         {
-            throw new System.NotImplementedException();
+            _db.Communicators.Add(_mapper.Map(fileCommunicator, new DAL.FileCommunicator()));
+            _db.SaveChanges();
         }
 
         public void UpdateFileCommunicator(DOM.FileCommunicator fileCommunicator)
         {
-            throw new System.NotImplementedException();
+            var dbValue = _db.Communicators.FirstOrDefault(comm => comm.Id == fileCommunicator.Id);
+            if (dbValue == null) return;
+            dbValue = _mapper.Map(fileCommunicator, new DAL.FileCommunicator());
+            _db.SaveChanges();
         }
 
         public void DeleteFileCommunicator(DOM.FileCommunicator fileCommunicator)
         {
-            throw new System.NotImplementedException();
+            var dbValue = RetrieveCommunicator(fileCommunicator.Id);
+            if (dbValue == null) return;
+            _db.Communicators.Remove(_mapper.Map(fileCommunicator, new DAL.FileCommunicator()));
+            _db.SaveChanges();
         }
 
         public void CreateSerialCommunicator(DOM.SerialCommunicator serialCommunicator)
         {
-            throw new System.NotImplementedException();
+            _db.Communicators.Add(_mapper.Map(serialCommunicator, new DAL.SerialCommunicator()));
+            _db.SaveChanges();
         }
 
         public DOM.Communicator RetrieveCommunicator(int id)
         {
             var dbValue = _db.Communicators.FirstOrDefault(comm => comm.Id == id);
             if (dbValue == null) return null;
-            return ConvertDALCommunicatorsToDOM(dbValue);
+            return ConvertDALCommunicatorToDOM(dbValue);
         }
 
         public ICollection<DOM.Communicator> RetrieveCommunicatorsForDevice(int deviceId)
         {
             return ConvertDALCommunicatorsToDOM(_db.Communicators.Where(comm => comm.Device.Id == deviceId)).ToList();
         }
+
+        public ICollection<DOM.Communicator> RetrieveAllCommunicators()
+        {
+            return ConvertDALCommunicatorsToDOM(_db.Communicators).ToList();
+        }
+
+        public void UpdateSerialCommunicator(DOM.SerialCommunicator serialCommunicator)
+        {
+            var dbValue = _db.Communicators.FirstOrDefault(comm => comm.Id == serialCommunicator.Id);
+            if (dbValue == null) return;
+            dbValue = _mapper.Map(serialCommunicator, new DAL.SerialCommunicator());
+            _db.SaveChanges();
+        }
+
+        public void DeleteSerialCommunicator(DOM.SerialCommunicator serialCommunicator)
+        {
+            var dbValue = RetrieveCommunicator(serialCommunicator.Id);
+            if (dbValue == null) return;
+            _db.Communicators.Remove(_mapper.Map(serialCommunicator, new DAL.SerialCommunicator()));
+            _db.SaveChanges();
+        }
+
+        public void CreateRule(DOM.Rule rule)
+        {
+            _db.Rules.Add(_mapper.Map(rule, new DAL.Rule()));
+            _db.SaveChanges();
+        }
+
+        public DOM.Rule RetrieveRule(int id)
+        {
+            return _mapper.Map(_db.Rules.FirstOrDefault(rule => rule.Id == id), new DOM.Rule());
+        }
+
+        public ICollection<DOM.Rule> RetrieveRulesForDevice(int deviceId)
+        {
+            return _db.Rules.Where(rule => rule.DeviceId == deviceId).Select(rule => _mapper.Map(rule, new DOM.Rule())).ToList();
+        }
+
+        public ICollection<DOM.Rule> RetrieveAllRules()
+        {
+            return _db.Rules.Select(rule => _mapper.Map(rule, new DOM.Rule())).ToList();
+        }
+
+        public void UpdateRule(DOM.Rule rule)
+        {
+            var dbValue = _db.Rules.FirstOrDefault(r=> r.Id==rule.Id);
+            if (dbValue == null) return;
+            dbValue = _mapper.Map(rule, new DAL.Rule());
+            _db.SaveChanges();
+        }
+
+        public void DeleteRule(DOM.Rule rule)
+        {
+            var dbValue = _db.Rules.FirstOrDefault(r => r.Id == rule.Id);
+            if (dbValue == null) return;
+            _db.Rules.Remove(dbValue);
+            _db.SaveChanges();
+        }
+
+        public void CreateValue(DOM.Value value)
+        {
+            _db.Values.Add(_mapper.Map(value, new DAL.Value()));
+            _db.SaveChanges();
+        }
+
+        public DOM.Value RetrieveValue(int id)
+        {
+            return _mapper.Map(_db.Values.FirstOrDefault(v => v.Id == id), new DOM.Value());
+        }
+
+        public ICollection<DOM.Value> RetrieveValuesForDevice(int deviceId)
+        {
+            return _db.Values.Where(v => v.Device.Id == deviceId).Select(v => _mapper.Map(v, new DOM.Value())).ToList();
+        }
+
+        public ICollection<DOM.Value> RetrieveAllValues()
+        {
+            return _db.Values.Select(v => _mapper.Map(v, new DOM.Value())).ToList();
+        }
+
+        public void UpdateValue(DOM.Value value)
+        {
+            var dbValue = _db.Values.FirstOrDefault(v => v.Id == value.Id);
+            if (dbValue == null) return;
+            dbValue = _mapper.Map(value, new DAL.Value());
+            _db.SaveChanges();
+        }
+
+        public void DeleteValue(DOM.Value value)
+        {
+            var dbValue = _db.Values.FirstOrDefault(v => v.Id == value.Id);
+            if (dbValue == null) return;
+            _db.Values.Remove(dbValue);
+            _db.SaveChanges();
+        }
+
+        public DOM.AppData GetApplicationInformation()
+        {
+            return _mapper.Map(_db.AppData.First(), new DOM.AppData());
+        }
+
+        public void SetApplicationInformation(DOM.AppData ai)
+        {
+            var dbValue = _db.AppData.First();
+            if (dbValue == null)
+            {
+                _db.AppData.Add(_mapper.Map(ai, new DAL.AppData()));
+            }
+            else
+            {
+                dbValue = _mapper.Map(ai, new DAL.AppData());
+            }
+
+            _db.SaveChanges();
+        }
+
+
+
+
+
+        ///
+        /// Methods below this point are PRIVATE and are used as 'helper methods'
+        /// for this class only. 
+        /// 
 
         /// <summary>
         /// Used to iteratively go through a list of communicator objects and convert them to Domain models.
@@ -236,94 +373,9 @@ namespace SCIPA.Data.Repository
                 case DAL.CommunicatorType.FlatFile:
                     return (_mapper.Map(communicator, new DOM.FileCommunicator()));
                 default:
-                    DebugOutput.Print("Unable to convert/understand Communicator...",communicator.Id.ToString());
+                    DebugOutput.Print("Unable to convert/understand Communicator...", communicator.Id.ToString());
                     return null;
             }
-        }
-
-        public ICollection<DOM.Communicator> RetrieveAllCommunicators()
-        {
-            return ConvertDALCommunicatorsToDOM(_db.Communicators).ToList();
-        }
-
-        public void UpdateSerialCommunicator(DOM.SerialCommunicator serialCommunicator)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public void DeleteSerialCommunicator(DOM.SerialCommunicator serialCommunicator)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public void CreateRule(DOM.Rule rule)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public DOM.Rule RetrieveRule(int id)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public ICollection<DOM.Rule> RetrieveRulesForDevice(int deviceId)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public ICollection<DOM.Rule> RetrieveAllRules()
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public void UpdateRule(DOM.Rule rule)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public void DeleteRule(DOM.Rule rule)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public void CreateValue(DOM.Value value)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public DOM.Value RetrieveValue(int id)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public ICollection<DOM.Value> RetrieveValuesForDevice(int deviceId)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public ICollection<DOM.Value> RetrieveAllValues()
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public void UpdateValue(DOM.Value value)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public void DeleteValue(DOM.Value value)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public DOM.AppData GetApplicationInformation()
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public void SetApplicationInformation(DOM.AppData ai)
-        {
-            throw new System.NotImplementedException();
         }
     }
 }
