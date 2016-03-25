e1968b45c143cc68e53084118448064a5334b230
Will Whitehead <whitehead.williamthomas@gmail.com>
2016-03-25 15:45:14 +0000

    Added all value types to Value.

--- SCIPA.Models/Domain Models/Value.cs
+++ SCIPA.Models/Domain Models/Value.cs
@@ -7,10 +7,14 @@ namespace SCIPA.Models
     {
         public int Id { get; set; }
         public System.DateTime EventTime { get; set; }
-        public string StringValue { get; set; }
         public bool Inbound { get; set; }
         public ValueType Type { get; set; }
-
-        public virtual Device Device { get; set; }
+        public int DeviceId { get; set; }
+        public Device Device { get; set; }
+        public int CommunicatorId { get; set; }
+        public int IntegerValue { get; set; }
+        public decimal FloatValue { get; set; }
+        public bool BooleanValue { get; set; }
+        public string StringValue { get; set; }
     }
 }
