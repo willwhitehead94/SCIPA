b4350f47c05ec8e9fd2f3a081b74d8e74989661f
Will Whitehead <whitehead.williamthomas@gmail.com>
2016-03-25 20:26:56 +0000

    Added MongoDb NuGet package.

--- SCIPA.Data.MongoLayer/Class1.cs
+++ SCIPA.Data.MongoLayer/Class1.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using  MongoDB.Bson;
+using MongoDB.Driver;
+using  MongoDB.Shared;
 
 namespace SCIPA.Data.MongoLayer
 {
