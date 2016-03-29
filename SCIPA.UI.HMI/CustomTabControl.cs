7dfe6899552e833e05cfefe6f9bf804823e6d0fa
Will Whitehead <whitehead.williamthomas@gmail.com>
2016-03-29 19:20:08 +0100

    Successful and working Dashboard before implementing domain logic.

--- SCIPA.UI.HMI/CustomTabControl.cs
+++ SCIPA.UI.HMI/CustomTabControl.cs
@@ -3,6 +3,13 @@ using System.Windows.Forms;
 
 namespace SCIPA.UI.HMI
 {
+    /// <summary>
+    /// Class implements and modifies C# code found on StackOverflow
+    /// at the following URL:
+    /// 
+    /// http://stackoverflow.com/questions/4912354/how-do-i-create-a-tabcontrol-with-no-tab-headers
+    /// See the chosen answer, by Cody Gray. Originally from Dot Net Thoughts (no longer active).
+    /// </summary>
     public class CustomTabControl : TabControl
 {
         /// <summary>
@@ -12,14 +19,15 @@ namespace SCIPA.UI.HMI
 
         protected override void WndProc(ref Message m)
         {
-            // Hide the tab headers at run-time
+            //When running the code during application run time, hide the
+            //headers.
             if (m.Msg == TCM_ADJUSTRECT && !DesignMode)
             {
                 m.Result = (IntPtr)1;
                 return;
             }
 
-            // call the base class implementation
+            //Calls the Windows Process method from the base class.
             base.WndProc(ref m);
         }
     }
