fe2c6cee768bc2309c4c42d8ebbbe15bb1d350ff
Will Whitehead <whitehead.williamthomas@gmail.com>
2016-03-29 18:23:48 +0100

    Added state flasshing to buttons on the Invoker.

--- SCIPA.UI.HMI/Dashboard.Designer.cs
+++ SCIPA.UI.HMI/Dashboard.Designer.cs
@@ -34,6 +34,12 @@
             this.lSeperator = new System.Windows.Forms.Label();
             this.lTime = new System.Windows.Forms.Label();
             this.lScipa = new System.Windows.Forms.Label();
+            this.bStartProcess = new System.Windows.Forms.Button();
+            this.bStopProcess = new System.Windows.Forms.Button();
+            this.bAddNew = new System.Windows.Forms.Button();
+            this.bModify = new System.Windows.Forms.Button();
+            this.bReports = new System.Windows.Forms.Button();
+            this.bAlarms = new System.Windows.Forms.Button();
             this.pHeader.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -62,6 +68,7 @@
             this.lHeaderAlarmCount.Size = new System.Drawing.Size(357, 51);
             this.lHeaderAlarmCount.TabIndex = 4;
             this.lHeaderAlarmCount.Text = "14 Active Alarms";
+            this.lHeaderAlarmCount.Click += new System.EventHandler(this.lHeaderAlarmCount_Click);
             // 
             // lDate
             // 
@@ -107,6 +114,72 @@
             this.lScipa.TabIndex = 0;
             this.lScipa.Text = "SCIPA";
             // 
+            // bStartProcess
+            // 
+            this.bStartProcess.BackColor = System.Drawing.Color.Gainsboro;
+            this.bStartProcess.Font = new System.Drawing.Font("Century Gothic", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bStartProcess.Location = new System.Drawing.Point(12, 124);
+            this.bStartProcess.Name = "bStartProcess";
+            this.bStartProcess.Size = new System.Drawing.Size(214, 85);
+            this.bStartProcess.TabIndex = 7;
+            this.bStartProcess.Text = "START DEVICE PROCESSES";
+            this.bStartProcess.UseVisualStyleBackColor = false;
+            // 
+            // bStopProcess
+            // 
+            this.bStopProcess.BackColor = System.Drawing.Color.Gainsboro;
+            this.bStopProcess.Font = new System.Drawing.Font("Century Gothic", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bStopProcess.Location = new System.Drawing.Point(232, 124);
+            this.bStopProcess.Name = "bStopProcess";
+            this.bStopProcess.Size = new System.Drawing.Size(214, 85);
+            this.bStopProcess.TabIndex = 8;
+            this.bStopProcess.Text = "STOP DEVICE PROCESSES";
+            this.bStopProcess.UseVisualStyleBackColor = false;
+            // 
+            // bAddNew
+            // 
+            this.bAddNew.BackColor = System.Drawing.Color.Gainsboro;
+            this.bAddNew.Font = new System.Drawing.Font("Century Gothic", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bAddNew.Location = new System.Drawing.Point(452, 124);
+            this.bAddNew.Name = "bAddNew";
+            this.bAddNew.Size = new System.Drawing.Size(214, 85);
+            this.bAddNew.TabIndex = 9;
+            this.bAddNew.Text = "ADD NEW DEVICE";
+            this.bAddNew.UseVisualStyleBackColor = false;
+            // 
+            // bModify
+            // 
+            this.bModify.BackColor = System.Drawing.Color.Gainsboro;
+            this.bModify.Font = new System.Drawing.Font("Century Gothic", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bModify.Location = new System.Drawing.Point(672, 124);
+            this.bModify.Name = "bModify";
+            this.bModify.Size = new System.Drawing.Size(214, 85);
+            this.bModify.TabIndex = 10;
+            this.bModify.Text = "MODIFY DEVICE";
+            this.bModify.UseVisualStyleBackColor = false;
+            // 
+            // bReports
+            // 
+            this.bReports.BackColor = System.Drawing.Color.Gainsboro;
+            this.bReports.Font = new System.Drawing.Font("Century Gothic", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bReports.Location = new System.Drawing.Point(892, 124);
+            this.bReports.Name = "bReports";
+            this.bReports.Size = new System.Drawing.Size(214, 85);
+            this.bReports.TabIndex = 11;
+            this.bReports.Text = "PROCESS REPORTS";
+            this.bReports.UseVisualStyleBackColor = false;
+            // 
+            // bAlarms
+            // 
+            this.bAlarms.BackColor = System.Drawing.Color.Gainsboro;
+            this.bAlarms.Font = new System.Drawing.Font("Century Gothic", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bAlarms.Location = new System.Drawing.Point(1112, 124);
+            this.bAlarms.Name = "bAlarms";
+            this.bAlarms.Size = new System.Drawing.Size(214, 85);
+            this.bAlarms.TabIndex = 12;
+            this.bAlarms.Text = "ALARMS";
+            this.bAlarms.UseVisualStyleBackColor = false;
+            // 
             // Dashboard
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
@@ -114,6 +187,12 @@
             this.BackgroundImage = global::SCIPA.UI.HMI.Properties.Resources.background;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(1338, 689);
+            this.Controls.Add(this.bAlarms);
+            this.Controls.Add(this.bReports);
+            this.Controls.Add(this.bModify);
+            this.Controls.Add(this.bAddNew);
+            this.Controls.Add(this.bStopProcess);
+            this.Controls.Add(this.bStartProcess);
             this.Controls.Add(this.pHeader);
             this.DoubleBuffered = true;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
@@ -121,6 +200,7 @@
             this.MinimizeBox = false;
             this.Name = "Dashboard";
             this.Text = "SCIPA Dashboard";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Dashboard_FormClosing);
             this.Load += new System.EventHandler(this.Dashboard_Load);
             this.pHeader.ResumeLayout(false);
             this.pHeader.PerformLayout();
@@ -136,6 +216,12 @@
         private System.Windows.Forms.Label lScipa;
         private System.Windows.Forms.Label lHeaderAlarmCount;
         private System.Windows.Forms.Label lSeperator;
+        private System.Windows.Forms.Button bStartProcess;
+        private System.Windows.Forms.Button bStopProcess;
+        private System.Windows.Forms.Button bAddNew;
+        private System.Windows.Forms.Button bModify;
+        private System.Windows.Forms.Button bReports;
+        private System.Windows.Forms.Button bAlarms;
     }
 }
 
