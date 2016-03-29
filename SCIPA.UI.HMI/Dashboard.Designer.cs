1df6e0d47d4c597d7469b891d14d377b9e33aaed
Will Whitehead <whitehead.williamthomas@gmail.com>
2016-03-29 17:24:46 +0100

    Added one second updates to the UI.

--- SCIPA.UI.HMI/Dashboard.Designer.cs
+++ SCIPA.UI.HMI/Dashboard.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace SCIPA.UI.HMI
 {
-    partial class HomePage
+    partial class Dashboard
     {
         /// <summary>
         /// Required designer variable.
@@ -29,11 +29,11 @@
         private void InitializeComponent()
         {
             this.pHeader = new System.Windows.Forms.Panel();
-            this.lScipa = new System.Windows.Forms.Label();
+            this.lHeaderAlarmCount = new System.Windows.Forms.Label();
             this.lDate = new System.Windows.Forms.Label();
-            this.lTime = new System.Windows.Forms.Label();
             this.lSeperator = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
+            this.lTime = new System.Windows.Forms.Label();
+            this.lScipa = new System.Windows.Forms.Label();
             this.pHeader.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -42,7 +42,7 @@
             this.pHeader.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.pHeader.BackColor = System.Drawing.Color.Black;
-            this.pHeader.Controls.Add(this.label1);
+            this.pHeader.Controls.Add(this.lHeaderAlarmCount);
             this.pHeader.Controls.Add(this.lDate);
             this.pHeader.Controls.Add(this.lSeperator);
             this.pHeader.Controls.Add(this.lTime);
@@ -52,16 +52,16 @@
             this.pHeader.Size = new System.Drawing.Size(1314, 106);
             this.pHeader.TabIndex = 0;
             // 
-            // lScipa
+            // lHeaderAlarmCount
             // 
-            this.lScipa.AutoSize = true;
-            this.lScipa.Font = new System.Drawing.Font("Century Gothic", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lScipa.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.lScipa.Location = new System.Drawing.Point(-12, -13);
-            this.lScipa.Name = "lScipa";
-            this.lScipa.Size = new System.Drawing.Size(364, 127);
-            this.lScipa.TabIndex = 0;
-            this.lScipa.Text = "SCIPA";
+            this.lHeaderAlarmCount.AutoSize = true;
+            this.lHeaderAlarmCount.Font = new System.Drawing.Font("Century Gothic", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lHeaderAlarmCount.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.lHeaderAlarmCount.Location = new System.Drawing.Point(761, 30);
+            this.lHeaderAlarmCount.Name = "lHeaderAlarmCount";
+            this.lHeaderAlarmCount.Size = new System.Drawing.Size(357, 51);
+            this.lHeaderAlarmCount.TabIndex = 4;
+            this.lHeaderAlarmCount.Text = "14 Active Alarms";
             // 
             // lDate
             // 
@@ -74,17 +74,6 @@
             this.lDate.TabIndex = 1;
             this.lDate.Text = "January 1st 2016";
             // 
-            // lTime
-            // 
-            this.lTime.AutoSize = true;
-            this.lTime.Font = new System.Drawing.Font("Century Gothic", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lTime.ForeColor = System.Drawing.SystemColors.ControlDark;
-            this.lTime.Location = new System.Drawing.Point(339, 50);
-            this.lTime.Name = "lTime";
-            this.lTime.Size = new System.Drawing.Size(190, 49);
-            this.lTime.TabIndex = 2;
-            this.lTime.Text = "01:03:42";
-            // 
             // lSeperator
             // 
             this.lSeperator.AutoSize = true;
@@ -96,18 +85,29 @@
             this.lSeperator.TabIndex = 3;
             this.lSeperator.Text = "|";
             // 
-            // label1
+            // lTime
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Century Gothic", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.SystemColors.ControlDark;
-            this.label1.Location = new System.Drawing.Point(761, 30);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(357, 51);
-            this.label1.TabIndex = 4;
-            this.label1.Text = "14 Active Alarms";
+            this.lTime.AutoSize = true;
+            this.lTime.Font = new System.Drawing.Font("Century Gothic", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lTime.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.lTime.Location = new System.Drawing.Point(339, 50);
+            this.lTime.Name = "lTime";
+            this.lTime.Size = new System.Drawing.Size(190, 49);
+            this.lTime.TabIndex = 2;
+            this.lTime.Text = "01:03:42";
             // 
-            // HomePage
+            // lScipa
+            // 
+            this.lScipa.AutoSize = true;
+            this.lScipa.Font = new System.Drawing.Font("Century Gothic", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lScipa.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lScipa.Location = new System.Drawing.Point(-12, -13);
+            this.lScipa.Name = "lScipa";
+            this.lScipa.Size = new System.Drawing.Size(364, 127);
+            this.lScipa.TabIndex = 0;
+            this.lScipa.Text = "SCIPA";
+            // 
+            // Dashboard
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
@@ -119,8 +119,9 @@
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
-            this.Name = "HomePage";
-            this.Text = "SCIPA";
+            this.Name = "Dashboard";
+            this.Text = "SCIPA Dashboard";
+            this.Load += new System.EventHandler(this.Dashboard_Load);
             this.pHeader.ResumeLayout(false);
             this.pHeader.PerformLayout();
             this.ResumeLayout(false);
@@ -133,7 +134,7 @@
         private System.Windows.Forms.Label lTime;
         private System.Windows.Forms.Label lDate;
         private System.Windows.Forms.Label lScipa;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lHeaderAlarmCount;
         private System.Windows.Forms.Label lSeperator;
     }
 }
--- SCIPA.UI.HMI/Program.cs
+++ SCIPA.UI.HMI/Program.cs
@@ -16,7 +16,7 @@ namespace SCIPA.UI.HMI
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new HomePage());
+            Application.Run(new Dashboard());
         }
     }
 }
