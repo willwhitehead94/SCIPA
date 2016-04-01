0e3aa8297bf9d05fafe9e109249cdcfa6c0eb7af
Will Whitehead <whitehead.williamthomas@gmail.com>
2016-04-01 23:49:12 +0100

    Removed unused references.

--- SCIPA.UI.HMI/Dashboard.Designer.cs
+++ SCIPA.UI.HMI/Dashboard.Designer.cs
@@ -1361,7 +1361,8 @@
             "Values by Device Overview",
             "Values by Device Report",
             "Device Values Over Time",
-            "Alarm Report"});
+            "Alarm Report",
+            "Export Devices and Values"});
             this.report_lbReports.Location = new System.Drawing.Point(53, 72);
             this.report_lbReports.Name = "report_lbReports";
             this.report_lbReports.Size = new System.Drawing.Size(713, 784);
--- SCIPA.UI.HMI/Dashboard.cs
+++ SCIPA.UI.HMI/Dashboard.cs
@@ -1,22 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
 using System.Drawing;
-using System.IO.Ports;
 using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using Microsoft.Reporting.WinForms;
-using Microsoft.ReportingServices.Interfaces;
 using SCIPA.Domain.Generic;
 using SCIPA.Domain.Inbound;
 using SCIPA.Domain.Logic;
 using SCIPA.Models;
 using Action = System.Action;
+using Application = System.Windows.Forms.Application;
 
 namespace SCIPA.UI.HMI
 {
@@ -1057,10 +1051,20 @@ namespace SCIPA.UI.HMI
                     reportPath = $"{reportPath}AlarmOverview";
                     break;
                 case 4:
-                    reportPath = $"{reportPath}/";
+                    string pkgLocation;
+                    Package pkg;
+                    Application app;
+                    DTSExecResult pkgResults;
+
+                    pkgLocation =
+                        @"C:\Users\Will Whitehead\Dropbox\University\Year 4\Computing Project\SCIPA\SCIPA.Domain.BI.Integration\ExportDeviceValuesToExcel.dtsx";
+                    app = new Application();
+                    pkg = app.LoadPackage(pkgLocation, null);
+                    pkgResults = pkg.Execute();
+
                     break;
                 case 5:
-                    reportPath = $"{reportPath}/";
+                    reportPath = $"{ reportPath}/";
                     break;
                 default:
                     break;
