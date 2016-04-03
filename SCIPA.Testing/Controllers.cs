bb1c764be623ffc02ba3ff0a030a05b1534392d2
Will Whitehead <whitehead.williamthomas@gmail.com>
2016-04-04 00:58:03 +0100

    Controller
    Controller update.

---
+++ SCIPA.Testing/Controllers.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MongoDB.Driver;
+using SCIPA.Domain.Generic;
+using SCIPA.Domain.Logic;
+using SCIPA.Models;
+
+namespace SCIPA.Testing
+{
+    [TestClass]
+    public class Controllers
+    {
+        /// <summary>
+        /// Ensures the key features of the Action controller are working as desired.
+        /// </summary>
+        [TestMethod]
+        public void ActionControllerKeyFunctionality()
+        {
+
+        }
+
+    }
+}
